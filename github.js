14abf928b9bacfcacbb48556f32140198188267c
Ruby-Jiang <xi.jiang@ucdconnect.ie>
2021-07-16 15:43:25 +0100

    requests with oauth

--- github.js
+++ github.js
@@ -7,13 +7,13 @@ class GitHub{
     }
 
     async getUser(user){
-        // const profileResponse = await fetch(`http://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const profileResponse = await fetch(`http://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-        // const reposResponse = await fetch(`http://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const reposResponse = await fetch(`http://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-        const profileResponse = await fetch(`http://api.github.com/users/${user}`);
+        // const profileResponse = await fetch(`http://api.github.com/users/${user}`);
 
-        const reposResponse = await fetch(`http://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`);
+        // const reposResponse = await fetch(`http://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`);
 
         const profileData = await profileResponse.json();
         const reposData = await reposResponse.json();
